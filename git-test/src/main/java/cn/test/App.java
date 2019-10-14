ea33afb31a8a998642b7bcd67d87f8c0d11cdd5c
zhuhongyuan <1941078431@qq.com>
2019-10-14 10:01:52 +0800

    测试gitcommit2

---
+++ git-test/src/main/java/cn/test/App.java
@@ -0,0 +1,14 @@
+package cn.test;
+
+/**
+ * @BelongsProject: test
+ * @BelongsPackage: cn.test
+ * @Author: Administrator
+ * @CreateTime: 2019-10-14 09:53
+ * @Description:
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("helloworld!!!");
+    }
+}
